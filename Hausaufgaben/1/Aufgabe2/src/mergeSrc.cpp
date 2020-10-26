966170233c4abd68b4f677af5c647f88a091574b
mathis-m <mathis.michel@outlook.de>
2020-10-26 01:44:22 +0100

    added merged src for easier debug

---
+++ Hausaufgaben/1/Aufgabe2/src/mergeSrc.cpp
@@ -0,0 +1,29 @@
+﻿#include <cmath>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+int basis, exponent;
+int basis2, exponent2;
+
+int main(void) {
+	//programm 2
+	cout << endl << "Basis = ";
+	cin >> basis;
+	cout << "Exponent = ";
+	cin >> exponent;
+	cout << endl << "Ergebnis: ";
+	cout << basis << exponent << " = "
+		<< pow(basis, exponent);
+
+	// programm 1
+	printf("\nBasis = ");
+	scanf_s("%d", &basis2);
+	printf("\nExponent = ");
+	scanf_s("%d", &exponent2);
+	printf("\nErgebnis: \n");
+	printf("\n%d^%d = %d\n", basis2, exponent2,
+		int(pow(double(basis2), double(exponent2))));
+
+	return 0;
+}
