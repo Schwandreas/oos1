141cc50fc221163a61ac21447edc9324c91be113
mathis-m <mathis.michel@outlook.de>
2020-10-26 01:28:56 +0100

    little refactor and comment what is going on

--- Hausaufgaben/Aufgabe1/src/setlocale.cpp
+++ Hausaufgaben/Aufgabe1/src/setlocale.cpp
@@ -7,33 +7,47 @@ using namespace std;
 int main()
 {
 	// define rawtime
-	time_t rawtime;
+	time_t rawtime = time(0);
 	// define timeinfo pointer
-	struct tm* timeinfo;
-	// set rawtime to cur time
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
+	struct tm timeinfo;
+	// fill tm struct with current time expressed for the local timezone
+	localtime_s(&timeinfo, &rawtime);
+	// init buffer
 	char buffer[80] = {0};
 
 	// Derzeitige Lokalitätseinstellungen augeben
+	// print locale // minimal === c
 	cout << "Locale ist: " << setlocale(LC_ALL,NULL) << endl;
-	strftime(buffer, 80, "%c", timeinfo);
+	// format timeinfo into buffer via 	"Preferred date and time stamp based on locale" convention
+	strftime(buffer, 80, "%c", &timeinfo);
+	// print time
 	cout << "Zeit: " << buffer;
+	// print char test => not working since minimal
 	cout << " Dies ist ein Test: üäöÜÖÄß. Ok?" << endl;
 
 	// Lokalitätseinstellungen der installierten Umgebung aktivieren
 	setlocale(LC_ALL, "");
+	// print cur locale
 	cout << "Locale ist: " << setlocale(LC_ALL,NULL) << endl;
-	strftime(buffer, 80, "%c", timeinfo);
+	// format time via "Preferred date and time stamp based on locale" convention with current locale from env
+	strftime(buffer, 80, "%c", &timeinfo);
+	// print
 	cout << "Zeit: " << buffer;
+	// print test wortking since locale ===  English_United States.1252
 	cout << " Dies ist ein Test: üäöÜÖÄß. Ok?" << endl;
 
 	// Einzelne Lokalitätseinstellungen ändern
+	// applies to time
 	setlocale(LC_TIME, "kor");
+	// This category applies to classification and conversion of characters, and to multibyte and wide characters
 	setlocale(LC_CTYPE, "jpn");
+	// print locale config kind of messed up now
 	cout << "Locale ist: " << setlocale(LC_ALL,NULL) << endl;
-	strftime(buffer, 80, "%c", timeinfo);
+	// put jpn time with kor char config into buffer
+	strftime(buffer, 80, "%c", &timeinfo);
+	// print buffer
 	cout << "Zeit: " << buffer;
+	// print not working text since kor 
 	cout << " Dies ist ein Test: üäöÜÖÄß. Ok?" << endl;
 
 	return 0;
