fa2f171ff6dcc09a9b8b337d310d0e7b20d98733
mathis-m <mathis.michel@outlook.de>
2020-10-26 03:14:05 +0100

    add aufgabe 3

---
+++ Hausaufgaben/1/Aufgabe3/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::cout << "Aufgabe 3:" << char(49);
+
+	const auto arrSize = 5;
+	int arr[arrSize] = {};
+	auto* const end = arr + arrSize;
+	std::string arrStr = "[";
+	std::string arrPlus1Str = "[";
+	std::string diffStr;
+	auto sum = 0;
+
+	for (auto* ptr = arr; ptr != end; ++ptr)
+	{
+		std::cout << "\nPlease enter an integer value: ";
+		std::cin >> *ptr;
+
+
+		auto spacerOrEnd = ']';
+		if (ptr + 1 != end)
+		{
+			spacerOrEnd = static_cast<char>(spacerOrEnd - '1');
+		}
+
+		arrStr += std::to_string(*ptr) + spacerOrEnd + " ";
+		arrPlus1Str += std::to_string(++(*ptr)) + spacerOrEnd + " ";
+		sum += *ptr;
+
+		if(ptr != arr)
+		{
+			diffStr += "\n" + std::to_string(*(ptr - 1)) + " - " + std::to_string(*ptr) + " = " + std::to_string(*(ptr - 1) - *ptr);
+		}
+	}
+	
+	std::cout << "\nSie haben folgendes Array eingegeben: " << arrStr;
+	std::cout << "\nJedes Item plus 1: " << arrPlus1Str;
+	std::cout << "\nDurchschnitt: " << sum / 5.;
+	std::cout << "\nDifferenzen der Nachbarn:" << diffStr;
+}
