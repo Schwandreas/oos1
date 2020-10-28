64339f7dd5b877653ec991f4f76d9075d98501a9
mathis-m <mathis.michel@outlook.de>
2020-10-28 14:41:00 +0100

    solved

--- Laboraufgaben/1/LaborAufgabe1/main.cpp
+++ Laboraufgaben/1/LaborAufgabe1/main.cpp
@@ -1,18 +1,20 @@
 #include <string>
-#include /* xxx */ 
-#include /* xxx */ 
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main() {
+int main()
+{
 	double d = 1.234;
 	cout << "Nr  Oct  Hex String  Fixed     Scientific " << endl;
-	for (int k = 1; k < 11; k++) {
-		cout << /* xxx */  << k;
-		cout << /* xxx */  << k;
-		cout << /* xxx */  << k << " ";
-		cout << /* xxx */  << string(k % 5 + 1, '*') << " ";
-		cout << /* xxx */  << d;
-		cout << /* xxx */  << d;
+	for (int k = 1; k < 11; k++)
+	{
+		cout << setfill(' ') << setw(3) << dec << k;
+		cout << setfill(' ') << setw(4) << oct << "0" + to_string(k);
+		cout << "  0x" << hex << k << " ";
+		cout << std::left << setfill('+') << setw(7) << string(k % 5 + 1, '*') << " ";
+		cout << std::right << ((d < 0) ? "- " : "+ ") << setfill(' ') << setw(7) << std::noshowpos << setprecision(3) << fixed << abs(d);
+		cout << std::noshowpos << setfill(' ') << setw(11) << scientific << d;
 		d = d * -2;
 		cout << endl;
 	}
