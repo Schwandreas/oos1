3dca920c6bb773399ca5cfb59e68120b7c1a0d7e
mathis-m <mathis.michel@outlook.de>
2020-11-01 18:39:29 +0100

    make readable

--- Laboraufgaben/1/LaborAufgabe2/Source.cpp
+++ Laboraufgaben/1/LaborAufgabe2/Source.cpp
@@ -8,22 +8,23 @@ int main(int argc, char* argv[])
 
 	std::cout << "Please enter the array size: ";
 	std::cin >> arrSize;
-	std::unique_ptr<OOS1Library::ArrayReader<int>> arrayReader(new OOS1Library::ArrayReader<int>(arrSize, "integer"));
-	arrayReader->readAll();
-	const std::unique_ptr<int[]> arr = std::move(arrayReader->arrayPtr);
+
+	const auto arr = std::make_unique<int[]>(arrSize);
 
 	std::string arrStr = "[";
 	auto        sum = 0;
-	std::string diffStr;
 	for (auto i = 0; i < arrSize; ++i)
 	{
+		std::cout << "\nPlease enter an integer value: ";
+		std::cin >> arr[i];
+
 		auto const spacerOrEnd = i + 1 != arrSize
 			? ','
 			: ']';
 		arrStr += std::to_string(arr[i]) + spacerOrEnd + " ";
 		sum += arr[i];
 	}
+
 	std::cout << "\nYour input was the following array: " << arrStr;
 	std::cout << "\nAverage: " << sum / static_cast<double>(arrSize);
-
 }
