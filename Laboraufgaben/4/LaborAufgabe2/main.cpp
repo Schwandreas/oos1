3ef9f7d25cf586bb3fb75e07b83ebd435ad2cc82
mathis-m <mathis.michel@outlook.de>
2021-01-25 03:12:27 +0100

    solve lab 4 2

--- Laboraufgaben/4/LaborAufgabe2/main.cpp
+++ Laboraufgaben/4/LaborAufgabe2/main.cpp
@@ -37,38 +37,38 @@ public:
 int main() {
 	A a;
 	B b;
-	cout << "a.f() "; 
-	a.f();    
-	cout << "b.f() "; 
-	b.f();     
-	cout << "a.g(1.2) "; 
-	a.g(1.2);  
-	cout << "b.g(1.2) "; 
-	b.g(1.2);  
-	cout << "a.g(1) "; 
-	a.g(1);    
-	cout << "b.g(1) "; 
-	b.g(1);    
-	cout << "a.g('a') "; 
-	a.g('a');  
-	cout << "b.g('a') "; 
-	b.g('a');  
-	cout << "a.g(\"a\") "; 
-	a.g("a");  
-	cout << "b.g(\"a\") "; 
-	b.g("a"); 
-	cout << "a.h(1) "; 
-	a.h(1);    
-	cout << "b.h(1) "; 
-	b.h(1);   
-	cout << "a.h('a') "; 
-	a.h('a');  
-	cout << "b.h('a') "; 
-	b.h('a'); 
-	cout << "a.h(\"a\") "; 
-	a.h("a");  
-	cout << "b.h(\"a\") "; 
-	b.h("a"); 
+	cout << "a.f() ";
+	a.f();
+	cout << "b.f() ";
+	b.f();
+	cout << "a.g(1.2) ";
+	a.g(1.2);
+	cout << "b.g(1.2) ";
+	b.g(1.2); // Überdeckt A::g(double d) 'double' to 'int' conversion
+	cout << "a.g(1) ";
+	a.g(1);
+	cout << "b.g(1) ";
+	b.g(1);   //Überdeckt A::g(double d)
+	cout << "a.g('a') ";
+	a.g('a');
+	cout << "b.g('a') ";
+	b.g('a'); //Überdeckt A::g(double d) 'char' to 'int' conversion ( a = 97 )
+	cout << "a.g(\"a\") ";
+	a.g("a"); //Überdeckt A::g(string) not assignable
+	//cout << "b.g(\"a\") ";
+	// b.g("a");
+	cout << "a.h(1) ";
+	a.h(1);
+	cout << "b.h(1) ";
+	b.h(1);   //Redefiniert A::h(string)
+	cout << "a.h('a') ";
+	a.h('a'); //
+	cout << "b.h('a') ";
+	b.h('a'); //Überdeckt A::h(char)
+	cout << "a.h(\"a\") ";
+	a.h("a"); //
+	cout << "b.h(\"a\") ";
+	b.h("a"); //Redefiniert A::h(string)
 	return 0;
 }
 
