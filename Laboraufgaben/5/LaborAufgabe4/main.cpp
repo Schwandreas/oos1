76d682408681049fa019595170ab88aa8fa2acf8
mathis-m <mathis.michel@outlook.de>
2021-02-01 00:59:23 +0100

    init(5.4)

---
+++ Laboraufgaben/5/LaborAufgabe4/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+class DrawingObject;
+
+int main()
+{
+	DrawingObject* objects[20];
+	int anzahl = 0;
+	std::cout << "Wieviele Objekte wollen Sie einlesen?" << std::endl;
+	std::cout << "Anzahl: ";
+	std::cin >> anzahl;
+	// Objekte einlesen
+	....
+		for (int i = 0; i < anzahl; i++)
+		{
+			// einzelnes Objekt einlesen
+			....
+				objects[i] = ....
+		}
+	// Objekte ausgeben
+	for (int i = 0; i < anzahl; i++)
+	{
+		// einzelnes Objekt ausgeben
+		....
+	}
+	return 0;
+}
